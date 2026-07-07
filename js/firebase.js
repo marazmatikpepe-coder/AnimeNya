d24477aae327af3049cc87827df175e4e084c9c7
marazmatikpepe-coder <marazmatikpepe@gmail.com>
2026-07-07 13:30:46 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,10 @@
-
+// Конфигурация Firebase (твой проект)
+export const firebaseConfig = {
+    apiKey: "AIzaSyBPwpR81prWq4_Ef1KX1T_uA4WsgyFMR8k",
+    authDomain: "akur-tv.firebaseapp.com",
+    databaseURL: "https://akur-tv-default-rtdb.firebaseio.com",
+    projectId: "akur-tv",
+    storageBucket: "akur-tv.firebasestorage.app",
+    messagingSenderId: "260087189248",
+    appId: "1:260087189248:web:2230651bb99a274bd86de5"
+};
